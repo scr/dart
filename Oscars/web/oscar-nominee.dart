af54091a6b39ff48d3e874a8bdf337ce94bbf327
Sheridan Rawlins <sheridan.rawlins@yahoo.com>
2014-02-27 20:45:14 -0800

    .

--- Oscars/web/oscar-nominee.dart
+++ Oscars/web/oscar-nominee.dart
@@ -10,10 +10,22 @@ import 'ui-filters.dart';
 @CustomTag('oscar-nominee')
 class OscarNominee extends PolymerElement {
   @published String name;
-  @published double points = 0.0;
-  @published bool checked = false;
+  double _points = 0.0;
+  bool _checked = false;
+  
+  @reflectable bool get checked => _checked;
+  @reflectable set checked(bool val) => 
+      _checked = notifyPropertyChange(#checked, _checked, val);
+  
+  @reflectable double get points => _points;
+  @reflectable set points(double val) => 
+      _points = notifyPropertyChange(#points, _points, val);
   
   final Transformer asDouble = new StringToDouble();
 
   OscarNominee.created() : super.created();
+  
+  nameClicked() {
+    checked = !_checked;
+  }
 }
